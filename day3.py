42e4dd2a1d520ef6f8714136631185aefb8a92a4
Yuresh Gurung <yureshgurung1600@gmail.com>
2025-08-31 06:21:57 +0545

    class python

---
+++ day3.py
@@ -0,0 +1,19 @@
+#complex data type
+a=3+4j
+b=5-2j
+# print(type(a))  # Prints <class 'complex'
+# print(a.real)  # Prints 3.0
+# print(a.imag)  # Prints 4.0 
+
+# print(a+2)  # Prints (5+4j)
+# print(a*2)  # Prints (6+8j)
+# print(a+b)  # Prints (8+2j)
+# print(a*b)  # Prints (23+2j)  
+print(abs(a))  # Prints 5.0 (magnitude of the complex number)
+print(a==b)  # Prints False
+print(a!=b)  # Prints True
+print(a**2)  # Prints (-7+24j) (square of the complex number)
+print(a/b)  # Prints (0.24+0.76j) (division
+print(pow(a,2))  # Prints (-7+24j) (a raised to the power of 2)
+print(complex(2,3))  # Prints (2+3j) (create complex number from real and imaginary parts)
+print(complex('1+2j'))  # Prints (1+2j) (
