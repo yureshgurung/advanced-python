0ab8914a0da83c1d0545ab611c8afc68aba96b45
Yuresh Gurung <yureshgurung1600@gmail.com>
2025-09-14 20:31:03 +0545

    update

---
+++ day7.py
@@ -0,0 +1,42 @@
+# a=input("enter 2 numbers:" )
+# print(a)
+# print(type(a))
+# b=a.split()
+# print(b)
+# print(type(b))
+# print(b[0])
+
+
+#why use split : separate the input value 
+# a=input("enter 2 numbers:" ).split()
+# print(a)
+
+
+# a=eval(input("enter numbers:" ))
+# print(a)
+# print(type(a))
+
+
+# command line arguments
+
+# from sys import argv
+# print(type(argv))
+# print(argv)
+# print(argv[1:])
+
+
+# #oupput
+# a=1
+# b=2
+# c=3
+# print("a=",a,"b=",b,"c=",c ,sep=" | ",end=" ")
+ 
+
+# print("hello",end=" ")
+# print("world",end=" ")
+# print("hi")
+name="john"
+age=23
+print("hello {} your age is {}".format(name,age))
+
+print(f"hello {name} your age is {age}")
