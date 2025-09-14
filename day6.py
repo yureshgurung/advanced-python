05d6c9cf92334dd7eece9a57edd0481da494ce3c
Yuresh Gurung <yureshgurung1600@gmail.com>
2025-09-14 16:42:47 +0545

    update

---
+++ day6.py
@@ -0,0 +1,34 @@
+#ternary operator 
+# syntax
+# <statement1> if <condition> else <statement2>
+# a=23
+# b=34
+# c=12 if a<b else 43
+# print(c)
+
+
+# a=12
+# # b=45
+# a="hello"
+# b="hello"
+
+# # print(id(a))
+# # print(id(b))
+# # print(a is b)
+# # print(a is not b)
+
+
+
+# a="yuresh gurung"
+# # print( 'y' in a)
+# a=1.2
+# b=1.2
+# print(a is b)
+
+import module as m
+# print(m.module(12,34))
+# print(m.module("hello ","world"))
+import math
+print(dir(math))
+print(math.sin(30))
+print(math.sqrt(3))
---
+++ module.py
@@ -0,0 +1,3 @@
+def module (a,b):
+    return a+b
+    
