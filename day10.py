60c46a22ec3a1af5282ea04aeb879d6e1205951b
Yuresh Gurung <yureshgurung1600@gmail.com>
2025-09-20 11:51:33 +0545

    update class

---
+++ day10.py
@@ -0,0 +1,55 @@
+#stirng
+#'',""
+#its not char all string  in pyhton
+
+# a='a'
+# g="yuresyhg"
+
+# print(type(a))
+# print(type(g))
+# #but are string
+
+# #multi line sting
+# #using''' .......'''
+# a='''my name is 
+# yuresh gurun'''
+# print(a)
+# a="""my name is 
+# yuresh gurun"""
+# print(a)
+
+
+# #escap sequence
+# s="my name is \n yuresh gurung"
+# print(s)
+
+# s="my name is \" yuresh gurung\""
+# print(s)
+# s="it\'s  a dog"
+# print(s)
+
+# #access the charaters of a string
+# # 1 indexing ,2 slicing
+# s="my name is yuresh"
+# i=0
+# print("index of given string in both positive and negativeindex")
+# for x in s:
+#     print(f"the charaters {x} of positve index is {i} and negative index is {i-len(s)}")
+#     i+=1
+
+
+
+#slice operator
+#beg-index:end index: step(0:n-1)
+s="yuresh is a good person"
+# print(s[0:5:2])
+# #inverse
+print(s[::-2])
+# b= reversed(s)
+# for x in b:
+#     print(x  , end="")
+
+
+
+
+
---
+++ day11 list.py
@@ -0,0 +1,30 @@
+#list:group of element single entity ,dullplication allowed, insertion oreder is preserved
+#properties: [],duplication allowed,insertion order is presevered(jun oder ma xanot change a oder ), heterogenous object are allowed,dynamic nature
+# list=[10,23,51,45,10,"radheradhe"]
+
+# # print(type(list))
+# list.append(500)
+# list.remove(10)
+# print(list)
+# a=[]
+# print(a)
+# l=eval(input("enter a list:"))
+# print(l)
+# print(type(l))
+
+#output:
+# enter a list:[10,23,45,63,41]
+# [10, 23, 45, 63, 41]
+# <class 'list'>
+
+# l=list(range(10,23))
+# l=list("yuresh")
+# print(l)
+# print(type(l))
+
+#list access by two method: index ,  slice
+l=[10,43,34,34,34]
+# print(l[4])
+# print(l[-2])
+print(l[5::-2])
+ 
