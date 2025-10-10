b267e20a5788d3b0d921a0e1fd562aa1a03d793b
Yuresh Gurung <yureshgurung1600@gmail.com>
2025-10-10 08:34:55 +0545

    python class

---
+++ day12-tuple.py
@@ -0,0 +1,50 @@
+t1=(3,5,6,3,3)
+t2=(4,56,6,4)
+# print(t1+t2)
+# # print(t1+(23)) #not support
+# print(t1+(23,)) 
+# print(t1*2)
+# print(t1==t2)#check element, order,number of element
+# print(3 in t1)#membership element
+# print(t1<t2)#first 3<4 ,5<56..........o
+# print(len(t1))
+# print(t1.count(3))
+# print(t1.index(3))#nabako lai chai error aauxa
+# r=reversed(t1)
+# print(r)
+# t=tuple(r)
+# print(t)
+
+# k=sorted(t1,reverse=True)
+# print(k)
+
+# print(min(t1),max(t1))
+
+
+
+# #packing and unpacking of tuple
+# a=9
+# b=3
+# c=5
+# t=a,b,c
+# print(t)
+# print(type(t))
+# #unpackingd
+# t2=(4,56,6,4)
+# a,b,c,d=t2
+# print(a,b,c,d)
+# a,*b=t2 #variable arg
+# print(a,b)#b chai list hunxa
+
+
+# #tuple comprehensive:not possible
+# t=(x*x for x in range(1,11,2))
+# print(type(t))
+
+
+
+
+
+
+
+
---
+++ day13-set-dict.py
@@ -0,0 +1,63 @@
+#if we want to represent a unique values
+#duplication are not allowed
+#insertion order is not preseved(so not applicable a indexing , slicing)
+#{}
+#set mutable
+#set heretogenous
+# s={} #dictionary ho hai empty bayo bane
+# s={12,34,54,"yuresh "}
+# print(s)
+ 
+
+# r=range(20)
+# s=set(r)
+# print(s)
+# s={23,34,23,45}#duplication not allowed
+# print(s)
+
+
+#indexing and slicing
+# print(s[:])#errror
+# s.add(84)
+# l=[34,65,23,45]
+# s.update(l)#update huna lai sequece wa itreative
+# print(s)
+
+# s.pop()
+# s.pop()
+# s.pop()
+# s.remove(34) #nabako lai error dinxa
+# s.discard(100)#no error 
+# print(s)
+
+
+# #mathmatical operation on set
+# s={23,34,23,45}
+# d={23,54,83,75}
+# print(s.union(d))
+# print(s.intersection(d))
+# print(s.difference(d))
+# print(s.symmetric_difference(d))
+#not valit : +,*, ==,!=,,< ......
+
+# #membership operator
+# print(23 in s)
+# print(23 not in s)
+
+
+
+
+
+
+# #set comprehenisve
+
+# s={x for x in range(34) if x%2==0}
+# print(s)
+ 
+
+
+
+
+
+#dictionary
+
