6255ff38f5c22a6869abb9c1448decd618203a1e
Yuresh Gurung <yureshgurung1600@gmail.com>
2025-10-14 23:58:41 +0545

    update class

---
+++ day14-fnction.py
@@ -0,0 +1,42 @@
+# #function
+# def yuresh():
+#     print("jay ram")
+
+# yuresh()
+
+
+
+# #two types of function= user, build
+# #build:
+# def ram():
+#     print("DFD")
+# ram()
+
+
+#return function
+# def ram():
+#     a=print("hi")
+#     return a
+# ram()
+
+# def ram(a,b):
+#     a=a+b
+#     return a
+# a=ram(2,3)
+# print(a)
+
+
+def ram(a,b): #formal parameter
+    a=a+b
+    r=a-b
+    return a ,r  #return a tuple
+a=ram(20,3)#actual parameter
+print(a)
+print(type(a))
+
+
+
+
+# types of argument: postional , keyword, defult,variable length argument
+
+
